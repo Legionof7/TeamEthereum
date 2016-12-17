516fe704674dd6522a2d781546051f8f35766c32
Legionof7 <justin.zheng@gmail.com>
2016-12-16 19:50:27 -0800

    Update dao.sol

--- dao.sol
+++ dao.sol
@@ -1,7 +1,16 @@
 pragma solidity ^0.4.2;
 /* The token is used as a voting shares */
-contract token { mapping (address => uint256) public balanceOf;  }
+contract token {
+    mapping (address => uint256) public voteWeight;
+    uint public numberOfDelegationRounds;
 
+    function balanceOf(address member) constant returns (uint256 balance) {
+        if (numberOfDelegationRounds < 3)
+            return 0;
+        else
+            return this.voteWeight(member);
+    }
+}
 
 /* define 'owned' */
 contract owned {
